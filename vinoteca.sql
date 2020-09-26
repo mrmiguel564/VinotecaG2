0998de1915989d10efd25c13a2671aabbd63058a
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2020-09-25 22:33:16 -0300

    comienzo de formulario

--- vinoteca.sql
+++ vinoteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2020 a las 02:47:41
+-- Tiempo de generación: 26-09-2020 a las 03:32:00
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
