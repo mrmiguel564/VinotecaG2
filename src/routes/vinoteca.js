82a7715f883096c44f0e2ec5d3b8bbf56504ff59
mauriciokiubi <63929790+mauriciokiubi@users.noreply.github.com>
2020-10-14 22:43:09 -0300

    cambie la a del admin

--- src/routes/vinoteca.js
+++ src/routes/vinoteca.js
@@ -17,7 +17,7 @@ router.get('/contacto', (req,res) => {
     res.render('contacto.ejs',{title: 'Pagina de contacto'});
 });
 
-router.get('/Admin', (req,res) => {
+router.get('/admin', (req,res) => {
     res.render('Admin.ejs',{title: 'Pagina del Administrador de Vinoteca'});
 });
 
