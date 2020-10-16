48eb58e6439395c4e8cd915c5ad24014e55048d3
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2020-10-16 20:28:24 -0300

    asdas

--- src/routes/vinoteca.js
+++ src/routes/vinoteca.js
@@ -6,12 +6,11 @@ const conn = require('../database'); // Buscando el archivo de conf de la base d
 router.get('/', (req,res) => {
     //res.render('index.ejs');
     conn.query('Select * from producto', (err,resp,campos) => {
-        console.log(resp);
+        //console.log(resp);
         res.render('vinoteca.ejs',   { datos: resp });
     });
     
 });
-
 
 router.get('/contacto', (req,res) => {
     res.render('contacto.ejs',{title: 'Pagina de contacto'});
@@ -20,6 +19,7 @@ router.get('/contacto', (req,res) => {
 router.get('/Admin', (req,res) => {
     res.render('Admin.ejs',{title: 'Pagina del Administrador de Vinoteca'});
 });
+
 
 //router.post('/ingresar',(req, res) => {
 //    //console.log(req.body);
