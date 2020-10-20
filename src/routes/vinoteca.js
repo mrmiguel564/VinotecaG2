9da21c47bbea0537f35edf701243351bc536288c
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2020-10-20 16:49:03 -0300

    asd

--- src/routes/vinoteca.js
+++ src/routes/vinoteca.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const conn = require('../database'); // Buscando el archivo de conf de la base de datos
