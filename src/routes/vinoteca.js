1f16eacb5e36c815881ff4f9dca05b3ed9cd5535
mauriciokiubi <mauricio.dejuan@alumnos.uv.cl>
2020-11-03 17:26:26 -0300

    prueba2

--- src/routes/vinoteca.js
+++ src/routes/vinoteca.js
@@ -12,7 +12,7 @@ router.get('/', (req,res) => {
     });
     
 });
-//hola 
+//hola2 
 router.post('/login',passport.authenticate('local',{
     successRedirect: "/correcto",
     failureRedirect: "/login"
