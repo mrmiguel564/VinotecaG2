8ea6ff182014ab47529b3b223944c753833207cc
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2020-11-03 18:05:19 -0300

    arregle los botones del formulario

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'vinotecaonline'
+    database: 'vinoteca'
 });
 
 conn.connect(function (err){
