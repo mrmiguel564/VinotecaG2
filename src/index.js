83e3b3442055cbd37b54e8fd21b2592195be02e6
mauriciokiubi <mauricio.dejuan@alumnos.uv.cl>
2020-11-10 01:16:46 -0300

    Trabajo terminado

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const conn = require('./database');
+const { json } = require('body-parser');
 const PassportLocal = require('passport-local').Strategy;
 
 // Configuración
@@ -26,19 +28,44 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new PassportLocal(function(username,password,done){
-<<<<<<< HEAD
-    if(username === "programacionweb" && password === "987654321")
-    
-     return done(null,{id:1, name: "Manuel"});
-=======
- 
-     
-     if(username === "programacionweb" && password === "987654321")
-    
-     return done(null,{id:1, name: "Manuel"});
+     conn.query('Select * from usuario where correo = ? and password = ?', [username, password], (err,resp,campos) =>{
+          try{
+               var user;
+               var pass;
+               var rol;
+                if(resp===null){
+                     user= "asd";
+                     pass ="asd";
+                }else{
+                     user = resp[0].correo
+                     pass = resp[0].password;
+                     rol  = resp[0].rol;
+                     module.exports = {
+                          correo: user,
+                          password1: pass,
+                          rol1: rol
+                     }
+                }
+            if(username === user && password ===pass){
+                 console.log("entre aqui")
+                 return done(null,{id:resp[0].correo, name: resp[0].nombre});
+                
+            }
+           }catch(e){
+                console.log("no tengo nada")
+                done(null,false);
+           }
 
-     done(null,false);
+
+          //if(username === "programacionweb" && password === "987654321")
+    
+     //return done(null,{id:1, name: "Manuel"});
+
+     //done(null,false);
+     });
 }));
+
+
     
 /*
      conn.query('Select * from usuario where correo = ? and password = ? and rol = "administrador"', [username, password] , (err,resp,campos) => {
@@ -50,19 +77,19 @@ passport.use(new PassportLocal(function(username,password,done){
     }
      }
 )})); 
-*/
 
->>>>>>> 4f7c23bda88c9a46f7ea8b1e99bd6a6ba23db268
+
 
      done(null,false);
 }));
+*/
 //Serialización, parar la información para identificar usuario en passport
 passport.serializeUser(function(user,done){
      done(null,user.id);
 });
 //Deserializacion
 passport.deserializeUser(function(id,done){
-     done(null, {id:1, name: "Manuel"})
+     done(null, {id:1, name: 'mauricio'})
 });
 
 
